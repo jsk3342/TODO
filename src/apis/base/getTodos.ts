50688fc14d908540949238354cb43a7d4c552843
jisu-kim <kss2460@naver.com>
2023-02-12 17:11:13 +0900

    refactor(getTodos): Todos 타입 추가

--- src/apis/base/getTodos.ts
+++ src/apis/base/getTodos.ts
@@ -1,8 +1,9 @@
 import { TODO_URL } from "../../constants/url";
+import { Todos } from "../../models/todo";
 import { fetchInstance } from "../instance/index";
 
 export const getTodos = async () => {
-  const { data } = await fetchInstance().get(TODO_URL);
+  const { data } = await fetchInstance().get<Todos>(TODO_URL);
 
   return data;
 };
