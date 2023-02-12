277329a038826b6c3574676993a41fc003ed6595
jisu-kim <kss2460@naver.com>
2023-02-12 17:15:03 +0900

    refactor(useGetTodos): 응답 데이터 처리 변경

--- src/queries/usefetchTodos.ts
+++ src/queries/usefetchTodos.ts
@@ -3,7 +3,11 @@ import { getTodos } from "../apis/base/getTodos";
 import { TODO_URL } from "../constants/url";
 
 export const useGetTodos = () => {
-  const { data } = useQuery(TODO_URL, () => getTodos());
-  const todoList = data.messages;
-  return todoList;
+  const { data } = useQuery({
+    queryKey: TODO_URL,
+    queryFn: () => getTodos(),
+    select: (data) => data.messages,
+  });
+
+  return data ?? [];
 };
