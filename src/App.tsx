55e6c90ce3d00ab49772721e78d40de14bb1d348
jisu-kim <kss2460@naver.com>
2023-02-12 17:09:26 +0900

    feat(App): 에러, 서스펜스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React, { useState } from "react";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import AppButton from "./App copy";
 import "./App.css";
-import Todo from "./pages/Todo";
+import Todolist from "./container/todolist";
 
 function App() {
   return (
     <>
       <AppButton />
-      <Todo />
+      <ErrorBoundary fallback={<div>에러가 발생했습니다.</div>}>
+        <Suspense fallback={<div>로딩 중입니다.</div>}>
+          <Todolist />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
