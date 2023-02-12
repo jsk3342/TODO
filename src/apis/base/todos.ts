fb215690e808a348c1258eaa2c260424efd4a8e1
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2023-02-12 13:44:31 +0900

    remove: todos

--- src/apis/base/todos.ts
+++
@@ -1,8 +0,0 @@
-import { TODO_URL } from "../../constants/url";
-import { fetchInstance } from "../instance/index";
-
-export const getTodos = async () => {
-  const { data } = await fetchInstance().get(TODO_URL);
-
-  return data;
-};
