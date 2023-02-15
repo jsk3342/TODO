e3e7862bd7191df2534fad0ccda7e15e7bea9aa6
jisu-kim <kss2460@naver.com>
2023-02-15 22:38:05 +0900

    feat(TodoForm): 참조 값 받는 인풋 추가 및 id 생성 추가하여 요청 형식에 맞게 대응

--- src/container/todolist/components/TodoForm.tsx
+++ src/container/todolist/components/TodoForm.tsx
@@ -1,63 +1,68 @@
-import { useRef } from "react";
+import React, { useCallback, useRef, useState } from "react";
 import styled from "styled-components";
 import { useAddTodoMutation } from "../../../queries/useAddTodos";
 
 export default function TodoForm() {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const { mutate } = useAddTodoMutation({
-    onSuccess: () => {
-      if (inputRef.current == null) {
-        return;
-      }
-
-      inputRef.current.value = "";
-    },
-  });
-
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-
-    if (inputRef.current == null || inputRef.current.value === "") {
-      return;
-    }
-
-    mutate({ title: inputRef.current.value });
-    inputRef.current.value = "";
-  };
-
-  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSubmit(e);
-    }
-  };
+  const [todo, setTodo] = useState("");
+  const [refId, setRefId] = useState("");
+  const { mutate: addTodoMutate } = useAddTodoMutation();
+  const nextId = useRef(4);
+  const handleOnClick = useCallback(() => {
+    addTodoMutate({
+      id: nextId.current,
+      title: todo,
+      refId: refId.split("").map(v => +v),
+      regTs: Date.now(),
+    });
+    setTodo("");
+    setRefId("")
+    nextId.current += 1;
+  }, [todo, addTodoMutate, refId]);
 
   return (
-    <form onSubmit={handleSubmit}>
-      <FlexBox>
-        <TodoInput ref={inputRef} onKeyDown={handleKeyDown} />
-        <TodoSubmitButton type="submit">Add Todo</TodoSubmitButton>
-      </FlexBox>
-    </form>
+    <FlexBox>
+      <InputWrapper>
+        <TodoInput placeholder="TODO 내용 입력" onChange={(e) => setTodo(e.target.value)} value={todo} />
+        <RefIdInput placeholder="참조 Id 입력 , 사용 ex) 1,2" onChange={(e) => setRefId(e.target.value)} value={refId} />
+      </InputWrapper>
+      <TodoSumitButton onClick={handleOnClick}>Add Todo</TodoSumitButton>
+    </FlexBox>
   );
 }
 
 const FlexBox = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 60px;
 `;
 
 const TodoInput = styled.input`
   height: 30px;
+  margin-bottom: 5px;
+  width: 100%;
   font-size: 20px;
   padding: 10px 20px;
-  flex-grow: 1;
   border: none;
   outline: none;
   border-radius: 5px;
   background-color: lightyellow;
 `;
 
-const TodoSubmitButton = styled.button`
+const InputWrapper = styled.div`
+  flex-grow: 1;
+`
+
+const RefIdInput = styled.input`
+  height: 30px;
+  width: 100%;
+  font-size: 20px;
+  padding: 10px 20px;
+  border: none;
+  outline: none;
+  border-radius: 5px;
+  background-color: lightyellow;
+`;
+
+const TodoSumitButton = styled.button`
   font-size: 25px;
   font-weight: bold;
   line-height: 25px;
