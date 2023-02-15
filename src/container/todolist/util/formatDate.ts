07476f6ede1a6d683b33328833afcf877692d7ea
jisu-kim <kss2460@naver.com>
2023-02-15 22:36:26 +0900

    feat(formatDate): 타임스태프 값 변환하여 날짜 형식 리턴

---
+++ src/container/todolist/util/formatDate.ts
@@ -0,0 +1,11 @@
+export default function formatDate(timestamp: number) {
+    const date = new Date(timestamp);
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
