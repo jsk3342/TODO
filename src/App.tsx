c186dfa9209be780feee5e8943625aa50ccbbfb1
jisu-kim <kss2460@naver.com>
2023-02-17 16:09:01 +0900

    remove: 안쓰는 코드 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-import AppButton from "./App copy";
-import "./App.css";
 import Todolist from "./container/todolist";
 import { StatusProvider } from "./container/todolist/contexts/StatusContext";
 
 function App() {
   return (
     <>
-      <AppButton />
       <ErrorBoundary fallback={<div>에러가 발생했습니다.</div>}>
         <Suspense fallback={<div>로딩 중입니다.</div>}>
           <StatusProvider>
